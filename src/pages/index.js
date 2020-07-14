3a2ab83d429d819c00f20d3144ddd35263726c98
Alexandr <alexandr.ponomarenko@icloud.com>
2020-07-15 01:16:02 +0300

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,11 @@ import Layout from "../components/layout"
 // import SEO from "../components/seo"
 import Post from "../components/post"
 
-const IndexPage = () => (
-  <Layout>
-    {/* <SEO title="Home" />
+const IndexPage = () => {
+  console.log(process.env.CONTENTFUL_SPACE_ID)
+  return (
+    <Layout>
+      {/* <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -17,24 +19,25 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
-    <StaticQuery
-      query={graphql`
-        {
-          allContentfulBlogPost {
-            edges {
-              node {
-                id
-                title
+      <StaticQuery
+        query={graphql`
+          {
+            allContentfulBlogPost {
+              edges {
+                node {
+                  id
+                  title
+                }
               }
             }
           }
+        `}
+        render={({ allContentfulBlogPost: { edges } }) =>
+          edges.map(({ node }) => <Post key={node.id} content={node} />)
         }
-      `}
-      render={({ allContentfulBlogPost: { edges } }) =>
-        edges.map(({ node }) => <Post key={node.id} content={node} />)
-      }
-    />
-  </Layout>
-)
+      />
+    </Layout>
+  )
+}
 
 export default IndexPage
